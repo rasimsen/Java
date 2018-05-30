7cc03c7898703a7bf5b3339152507c121d742870
Rasim Şen <rasimsen@gmail.com>
2018-05-30 17:01:33 +0100

    Business Rule Manegement System - Drools - SpringBoot

---
+++ BusinessRuleManagementSystem-Drools/SpringBoot-Drools-Demo/src/main/java/com/oasissofttech/controller/JewelleryShopController.java
@@ -0,0 +1,34 @@
+package com.oasissofttech.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.oasissofttech.model.Product;
+import com.oasissofttech.service.JewelleryShopService;
+
+
+
+@RestController
+public class JewelleryShopController {
+
+	private final JewelleryShopService jewelleryShopService;
+
+	@Autowired
+	public JewelleryShopController(JewelleryShopService jewelleryShopService) {
+		this.jewelleryShopService = jewelleryShopService;
+	}
+
+	@RequestMapping(value = "/getDiscount", method = RequestMethod.GET, produces = "application/json")
+	public Product getQuestions(@RequestParam(required = true) String type) {
+
+		Product product = new Product();
+		product.setType(type);
+		jewelleryShopService.getProductDiscount(product);
+		return product;
+	}
+}
